c1249c03d3445816667b5954f15f7351fcdd0d07
jacob <2staffi@gmail.com>
2016-10-09 18:18:55 -0400

    fix get

--- app.rb
+++ app.rb
@@ -3,3 +3,71 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+get '/404' do
+    erb :404
+end
+
+get '/about' do
+    erb :about
+end
+
+get '/blog_home_1' do
+    erb :blog_home_1
+end
+
+get '/blog_home_2' do
+    erb :blog_home_2
+end
+
+get '/blog_post' do
+    erb :blog_post
+end
+
+get '/contact' do
+    erb :contact
+end
+
+get '/faq' do
+    erb :faq
+end
+
+get '/full_width' do
+    erb :full_width
+end
+
+get '/portfolio_1_col' do
+    erb :portfolio_1_col
+end
+
+get '/portfolio_2_col' do
+    erb :portfolio_2_col
+end
+
+get '/portfolio_3_col' do
+    erb :portfolio_3_col
+end
+
+get '/portfolio_4_col' do
+    erb :portfolio_4_col
+end
+
+get '/about' do
+    erb :about
+end
+
+get '/about' do
+    erb :about
+end
+
+get '/about' do
+    erb :about
+end
+
+get '/about' do
+    erb :about
+end
+
+get '/about' do
+    erb :about
+end
